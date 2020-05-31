7c509730e006b690039351bef7f419b2c1e4c2c1
kyp <kyp@kmc.gr.jp>
2020-05-31 21:13:53 +0900

    kari

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './common/Header';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
--- client/src/common/Header.tsx
+++ client/src/common/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
 
-import './Header.css';
+import './Header.scss';
 import logo from './logomark_color.png';
 
 export default function Header() {
